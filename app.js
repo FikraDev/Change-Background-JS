c5dfd08c0e50eedd49191cf73453dbc6fba2fd84
fikradev <sachel.campbell@gmail.com>
2021-03-12 13:55:40 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const backgroundx = document.querySelector('.rootdiv');
+
+const buttonx = document.querySelector('button');
+
+buttonx.addEventListener('click', ()=>{
+
+    // backgroundx.style.backgroundColor='green'; //option 
+    backgroundx.classList.toggle('purrple'); //option 2
+    
+});
+
